17742cd6795c8fafdccd07ed9da5b2d6cf91560c
Wemerson Jesus <wejesuss@gmail.com>
2021-06-02 10:45:16 -0300

    feat: add dummy graph

---
+++ js/charts.js
@@ -0,0 +1,41 @@
+import { createChart } from "lightweight-charts";
+
+const container = document.getElementById("c-main__chart");
+const chart = createChart(container, {
+  width: 400,
+  height: 300,
+  timeScale: { barSpacing: 30 },
+});
+
+const lineSeries2 = chart.addLineSeries({
+  color: "gold",
+});
+
+const lineSeries = chart.addLineSeries();
+lineSeries.setData([
+  { time: "2019-04-11", value: 80.01 },
+  { time: "2019-04-12", value: 96.63 },
+  { time: "2019-04-13", value: 76.64 },
+  { time: "2019-04-14", value: 81.89 },
+  { time: "2019-04-15", value: 74.43 },
+  { time: "2019-04-16", value: 80.01 },
+  { time: "2019-04-17", value: 96.63 },
+  { time: "2019-04-18", value: 76.64 },
+  { time: "2019-04-19", value: 81.89 },
+  { time: "2019-04-20", value: 74.43 },
+]);
+
+lineSeries2.setData([
+  { time: "2019-04-10", value: 70.01 },
+  { time: "2019-04-12", value: 236.63 },
+  { time: "2019-04-13", value: 326.64 },
+  { time: "2019-04-14", value: 11.89 },
+  { time: "2019-04-15", value: 44.43 },
+  { time: "2019-04-16", value: 560.01 },
+  { time: "2019-04-17", value: 86.63 },
+  { time: "2019-04-18", value: 16.64 },
+  { time: "2019-04-19", value: 51.89 },
+  { time: "2019-04-20", value: 44.43 },
+]);
+
+console.log(lineSeries);
